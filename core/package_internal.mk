2fb29c49781772479bfdf73efa5c43339d15caa3
chris4824 <chris482422@gmail.com>
2016-09-27 12:50:54 -0400

    Revert "Revert "build: Add build flag to disable CMSDK usage""
    This reverts commit 2b5625299918f2b1d28fe6589b3c2c5cc9b44674.

--- core/package_internal.mk
+++ core/package_internal.mk
@@ -436,13 +436,15 @@ framework_res_package_export_deps := $(framework_res_package_export)
 else # LOCAL_SDK_RES_VERSION
 framework_res_package_export := \
     $(call intermediates-dir-for,APPS,framework-res,,COMMON)/package-export.apk
-
+ifneq ($(TARGET_DISABLE_CMSDK), true)
+endif
 # We can't depend directly on the export.apk file; it won't get its
 # PRIVATE_ vars set up correctly if we do.  Instead, depend on the
 # corresponding R.stamp file, which lists the export.apk as a dependency.
 framework_res_package_export_deps := \
     $(dir $(framework_res_package_export))src/R.stamp
-
+ifneq ($(TARGET_DISABLE_CMSDK), true)
+endif
 endif # LOCAL_SDK_RES_VERSION
 all_library_res_package_exports := \
     $(framework_res_package_export) \
@@ -453,7 +455,8 @@ all_library_res_package_export_deps := \
     $(framework_res_package_export_deps) \
     $(foreach lib,$(LOCAL_RES_LIBRARIES),\
         $(call intermediates-dir-for,APPS,$(lib),,COMMON)/src/R.stamp)
-
+ifneq ($(TARGET_DISABLE_CMSDK), true)
+endif
 $(resource_export_package) $(R_file_stamp) $(LOCAL_BUILT_MODULE): $(all_library_res_package_export_deps)
 $(LOCAL_INTERMEDIATE_TARGETS): \
     PRIVATE_AAPT_INCLUDES := $(all_library_res_package_exports)
